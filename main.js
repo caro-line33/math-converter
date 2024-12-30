aa5608de929e249fe1c34f37e42886aa3075d84a
Caroline <huang244@msu.edu>
2024-12-30 15:09:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,12 +8,13 @@ import "https://unpkg.com/@cortex-js/compute-engine?module"
 const options = document.getElementById("options")
 const output = document.getElementById('output')
 const formula = document.getElementById('formula')
+const copy = document.getElementById('copy')
 let convertTo = options.value;
 
 formula.addEventListener('input', convert);
 options.addEventListener('change', variable);
 options.addEventListener('change', convert);
-
+copy.addEventListener('click', copyOutput);
 
 function variable(){
     convertTo = options.value; 
@@ -22,6 +23,11 @@ function variable(){
 function convert(){
     output.innerHTML = formula.getValue(convertTo);
 }
+
+function copyOutput(){
+    navigator.clipboard.writeText(output.value);
+}
+
 
 
 
